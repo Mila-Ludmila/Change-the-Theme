d8c36e31d48da7a8e047a6c022ac536ce0673878
Mila-Ludmila <118370806+Mila-Ludmila@users.noreply.github.com>
2023-10-15 19:02:11 +0300

    Update index.js
    change to files index.js

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
   const themeToggle = document.getElementById("theme-toggle");
   const themeStylesheet = document.getElementById("theme-stylesheet");
-
-  // Check localStorage for user preference
   const userTheme = localStorage.getItem("style");
-
-  // Apply user preference or default to light theme
   if (userTheme === "dark") {
       themeStylesheet.href = "style-dark.css";
   }
-
-  // Theme toggle button click event
   themeToggle.addEventListener("click", function () {
       if (themeStylesheet.href.includes("light")) {
           themeStylesheet.href = "style-dark.css";
